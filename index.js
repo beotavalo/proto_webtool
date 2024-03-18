0c065d85e0ed6807206c5596d6e59d0df0048419
Braulio Otavalo <braulio.otavalo@gmail.com>
2024-03-18 10:02:43 -0500

    Complete map with layer control, legend and color change

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-var mapa = L.map("contenedor-del-mapa").setView([-1.8, -78], 6.5) //-78, -1.8, 6
-//L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png?", {}).addTo(mapa)
+var mapa = L.map("contenedor-del-mapa").setView([-1.8, -78], 6.5) 
 
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?', {
-	minZoom: 5,
-	maxZoom: 8
+var osm  = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png?', {
+    minZoom: 6,
+    maxZoom: 8,
+    attribution: '© OpenStreetMap'
 }).addTo(mapa);
 
- 
+var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    minZoom: 6,
+    maxZoom: 8,
+    attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
 
-/*
-var marcador = L.marker([-0.3, -78.49]).addTo(mapa)
-//marcador.bindPopup("Ferrocarriles de Ecuador Longitud: 52 Km")
+// Insertando una leyenda en el mapa
+var legend = L.control({ position: 'bottomright' });
 
-var popup = L.popup()
-    .setLatLng([-0.3, -78.49])
-    .setContent('<H3> P1: Metro labrador </h3> <h4> Longitud: 10 Km </h4> <h4> CAPEX: 100 M USD </h4> ')
-    .addTo(mapa);
+legend.onAdd = function (mapa) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML = `<svg height="100" width="120">
+    <!-- Color de fondo -->
+    <rect width="100%" height="100%" fill="#f0f0f0"></rect>
+    <text x="10" y="10" style="font-family: sans-serif; font-size: 12px;">LEYENDA</text>
+    <rect x="10" y="20" height="12" width="12" style="stroke-width: 4; stroke: #0000; fill: #20bf6b; fill-opacity: 0.5;"></rect>
+    <text x="25" y="30" style="font-family: sans-serif; font-size: 12px;">Alta prioridad</text>
+    <rect x="10" y="40" height="12" width="12" style="stroke-width: 4; stroke: #0000; fill: #45aaf2; fill-opacity: 0.5;"></rect>
+    <text x="25" y="50" style="font-family: sans-serif; font-size: 12px;">Prioridad SE</text>
+    <rect x="10" y="60" height="12" width="12" style="stroke-width: 4; stroke: #0000; fill: #a55eea; fill-opacity: 0.5;"></rect>
+    <text x="25" y="70" style="font-family: sans-serif; font-size: 12px;">Prioridad FE</text>
+    <rect x="10" y="80" height="12" width="12" style="stroke-width: 4; stroke: #0000; fill: #fc5c65; fill-opacity: 0.5;"></rect>
+    <text x="25" y="90" style="font-family: sans-serif; font-size: 12px;">Menor prioridad</text>
+    </svg>`;
+    return div;
+};
+
+legend.addTo(mapa);
 
 
-const circulo = L.circle([-1.2, -78.65], {
-    radius: 1000,
-    color: "green"
-}).addTo(mapa)
-circulo.bindPopup("Extensión Metro Labrador - La Ofelia Longitud: 4 Km")
 
-function clicSobreMapa(evento) {
-    alert("Diste clic en el punto con coordenadas latitud: " + evento.latlng.lat + " y longitud: " + evento.latlng.lng)
-}
 
-mapa.on("click", clicSobreMapa);
-
-console.log(mapa)
-console.log(marcador)
-console.log(circulo)*/
